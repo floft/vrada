Fixed multi-class output, fixed accuracy calculation
- Previously was still outputting softmax when multi-class
- Accuracy calculation for multi-class is when sigmoid >0.5
- Accuracy calculation for single-class predictions is an accuracy for
  each class

VRADA.py
@@ -44,8 +44,8 @@ def compute_evaluation(sess,
     """
     # Evaluation set in batches (since it may not fit into memory
     # all at once)
-    task_a = 0
-    task_b = 0
+    task_a = np.zeros((num_classes,), dtype=np.float32)
+    task_b = np.zeros((num_classes,), dtype=np.float32)
     domain_a = 0
     domain_b = 0
     a_total = 0
@@ -307,8 +307,20 @@ def train(data_info,
     # run these multiple times if we can't fit the entire validation set into
     # memory. Then afterwards we can divide by the size of the validation set.
     with tf.variable_scope("task_accuracy"):
-        # Accuracy for each class
-        equals = tf.cast(tf.equal(y, task_classifier), tf.float32)
+        # If multi-class, then each output is a sigmoid independent of the others,
+        # so for each class check >0.5 for predicting a "yes" for that class.
+        if multi_class:
+            equals = tf.cast(
+                tf.equal(y, tf.cast(tf.greater(task_classifier, 0.5), tf.float32)),
+                tf.float32)
+        # If only predicting a single class (using softmax), then look for the
+        # max value
+        else:
+            # e.g. [0.2 0.2 0.4 0.2] -> [0 0 1 0]
+            pred_one_hot = tf.one_hot(tf.argmax(task_classifier, axis=-1), num_classes)
+            # compare predicted one_hot and ground truth for each class
+            equals = tf.cast(tf.equal(y, pred_one_hot), tf.float32)
+
         task_accuracy_sum = tf.reduce_sum(equals, axis=0)
         task_accuracy = tf.reduce_mean(equals, axis=0)
     with tf.variable_scope("domain_accuracy"):
@@ -356,13 +368,14 @@ def train(data_info,
         tf.summary.scalar("accuracy_domain/target/training", domain_accuracy)
     ]
     for i in range(num_classes):
+        with tf.variable_scope("task_accuracy"):
+            class_acc = tf.reshape(tf.slice(task_accuracy, [i], [1]), [])
+
         training_a_summs += [
-            tf.summary.scalar("accuracy_task_%d/source/training" % i,
-                tf.slice(task_accuracy, [i], [1])),
+            tf.summary.scalar("accuracy_task_%d/source/training" % i, class_acc),
         ]
         training_b_summs += [
-            tf.summary.scalar("accuracy_task_%d/target/training" % i,
-                tf.slice(task_accuracy, [i], [1])),
+            tf.summary.scalar("accuracy_task_%d/target/training" % i, class_acc),
         ]
     training_summaries_a = tf.summary.merge(training_a_summs)
     training_summaries_extra_a = tf.summary.merge(model_summaries)
@@ -471,10 +484,12 @@ def train(data_info,
                     x, y, task_classifier, domain, keep_prob, training,
                     batch_size, auc_labels, auc_predictions, task_auc_all)
 
-                task_source_val = tf.Summary(value=[tf.Summary.Value(
-                    tag="accuracy_task/source/validation",
-                    simple_value=task_a_accuracy
-                )])
+                task_source_val = []
+                for i in range(num_classes):
+                    task_source_val += [tf.Summary(value=[tf.Summary.Value(
+                        tag="accuracy_task_%d/source/validation" % i,
+                        simple_value=task_a_accuracy[i]
+                    )])]
                 domain_source_val = tf.Summary(value=[tf.Summary.Value(
                     tag="accuracy_domain/source/validation",
                     simple_value=domain_a_accuracy
@@ -483,10 +498,12 @@ def train(data_info,
                     tag="auc_task/source/validation",
                     simple_value=task_a_auc
                 )])
-                task_target_val = tf.Summary(value=[tf.Summary.Value(
-                    tag="accuracy_task/target/validation",
-                    simple_value=task_b_accuracy
-                )])
+                task_target_val = []
+                for i in range(num_classes):
+                    task_target_val += [tf.Summary(value=[tf.Summary.Value(
+                        tag="accuracy_task_%d/target/validation" % i,
+                        simple_value=task_b_accuracy[i]
+                    )])]
                 domain_target_val = tf.Summary(value=[tf.Summary.Value(
                     tag="accuracy_domain/target/validation",
                     simple_value=domain_b_accuracy
@@ -495,10 +512,12 @@ def train(data_info,
                     tag="auc_task/target/validation",
                     simple_value=task_b_auc
                 )])
-                writer.add_summary(task_source_val, step)
+                for s in task_source_val:
+                    writer.add_summary(s, step)
                 writer.add_summary(domain_source_val, step)
                 writer.add_summary(task_source_auc_val, step)
-                writer.add_summary(task_target_val, step)
+                for s in task_target_val:
+                    writer.add_summary(s, step)
                 writer.add_summary(domain_target_val, step)
                 writer.add_summary(task_target_auc_val, step)
 

model.py
@@ -38,6 +38,8 @@ def classifier(x, num_classes, keep_prob, training, batch_norm):
     Returns both output without applying softmax for use in loss function
     and after for use in prediction. See softmax_cross_entropy_with_logits_v2
     documentation: "This op expects unscaled logits, ..."
+
+    Also returns sigmoid output for if doing multi-class classification.
     """
     classifier_output = x
     num_layers = 4
@@ -62,9 +64,10 @@ def classifier(x, num_classes, keep_prob, training, batch_norm):
             if i != num_layers-1:
                 classifier_output = tf.nn.relu(classifier_output)
 
+    sigmoid_output = tf.nn.sigmoid(classifier_output)
     softmax_output = tf.nn.softmax(classifier_output)
 
-    return classifier_output, softmax_output
+    return classifier_output, softmax_output, sigmoid_output
 
 def build_model(x, y, domain, grl_lambda, keep_prob, training,
         num_classes, adaptation=True, multi_class=False, batch_norm=False,
@@ -105,21 +108,21 @@ def build_model(x, y, domain, grl_lambda, keep_prob, training,
 
     # Pass last output to fully connected then softmax to get class prediction
     with tf.variable_scope("task_classifier"):
-        task_classifier, task_softmax = classifier(
+        task_classifier, task_softmax, task_sigmoid = classifier(
             feature_extractor, num_classes, keep_prob, training, batch_norm)
 
     # Also pass output to domain classifier
     # Note: always have 2 domains, so set outputs to 2
     with tf.variable_scope("domain_classifier"):
         gradient_reversal_layer = flip_gradient(feature_extractor, grl_lambda)
-        domain_classifier, domain_softmax = classifier(
+        domain_classifier, domain_softmax, _ = classifier(
             gradient_reversal_layer, 2, keep_prob, training, batch_norm)
 
     # Maybe try one before the feature extractor too
     if two_domain_classifiers:
         with tf.variable_scope("domain_classifier2"):
             gradient_reversal_layer2 = flip_gradient(x, grl_lambda)
-            domain_classifier2, _ = classifier(
+            domain_classifier2, _, _ = classifier(
                 gradient_reversal_layer2, 2, keep_prob, training, batch_norm)
 
     # If doing domain adaptation, then we'll need to ignore the second half of the
@@ -146,6 +149,9 @@ def build_model(x, y, domain, grl_lambda, keep_prob, training,
             task_softmax = tf.cond(training,
                 lambda: tf.slice(task_softmax, [0, 0], [batch_size // 2, -1]),
                 lambda: task_softmax)
+            task_sigmoid = tf.cond(training,
+                lambda: tf.slice(task_sigmoid, [0, 0], [batch_size // 2, -1]),
+                lambda: task_sigmoid)
             y = tf.cond(training,
                 lambda: tf.slice(y, [0, 0], [batch_size // 2, -1]),
                 lambda: y)
@@ -170,6 +176,12 @@ def build_model(x, y, domain, grl_lambda, keep_prob, training,
             domain_loss += tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(
                 labels=tf.stop_gradient(domain), logits=domain_classifier2))
 
+    # If multi-class the task output will be sigmoid rather than softmax
+    if multi_class:
+        task_output = task_sigmoid
+    else:
+        task_output = task_softmax
+
     # Extra summaries
     summaries = [
         tf.summary.scalar("loss/task_loss", task_loss),
@@ -185,10 +197,10 @@ def build_model(x, y, domain, grl_lambda, keep_prob, training,
         for i in range(num_classes):
             summaries += [
                 tf.summary.histogram("outputs/task_classifier_%d" % i,
-                    tf.slice(task_softmax, [i], [1]))
+                    tf.slice(task_output, [0,i], [tf.shape(task_output)[0],1]))
             ]
 
-    return task_softmax, domain_softmax, task_loss, domain_loss, \
+    return task_output, domain_softmax, task_loss, domain_loss, \
         feature_extractor, summaries
 
 def build_lstm(x, y, domain, grl_lambda, keep_prob, training,
@@ -205,7 +217,7 @@ def build_lstm(x, y, domain, grl_lambda, keep_prob, training,
         rnn_output = outputs[:, -1]
 
     # Other model components passing in output from RNN
-    task_softmax, domain_softmax, task_loss, domain_loss, \
+    task_output, domain_softmax, task_loss, domain_loss, \
         feature_extractor, summaries = build_model(
             rnn_output, y, domain, grl_lambda, keep_prob, training,
             num_classes, adaptation, multi_class)
@@ -220,7 +232,7 @@ def build_lstm(x, y, domain, grl_lambda, keep_prob, training,
     # We can't generate with an LSTM
     extra_outputs = None
 
-    return task_softmax, domain_softmax, total_loss, \
+    return task_output, domain_softmax, total_loss, \
         feature_extractor, summaries, extra_outputs
 
 def build_vrnn(x, y, domain, grl_lambda, keep_prob, training,
@@ -254,7 +266,7 @@ def build_vrnn(x, y, domain, grl_lambda, keep_prob, training,
             rnn_output = h[:,-1]
 
     # Other model components passing in output from RNN
-    task_softmax, domain_softmax, task_loss, domain_loss, \
+    task_output, domain_softmax, task_loss, domain_loss, \
         feature_extractor, summaries = build_model(
             rnn_output, y, domain, grl_lambda, keep_prob, training,
             num_classes, adaptation, multi_class)
@@ -322,5 +334,5 @@ def build_vrnn(x, y, domain, grl_lambda, keep_prob, training,
         decoder_mu, decoder_sigma,
     ]
 
-    return task_softmax, domain_softmax, total_loss, \
+    return task_output, domain_softmax, total_loss, \
         feature_extractor, summaries, extra_outputs